3003cb1fe4f26177bb305165085f105186d8640e
philipwerner <philip.r.werner@gmail.com>
2018-03-21 21:17:43 -0700

    Finishing up the readme

--- BankATM/BankATM/Program.cs
+++ BankATM/BankATM/Program.cs
@@ -6,11 +6,16 @@ namespace BankATM
     {
         static void Main(string[] args)
         {
-            decimal balance = 2000M;
+            decimal balance = 2000M;    //starting user balance
             Console.WriteLine("Welcome To Your Money");
             UserInterface(balance);
         }
 
+        /// <summary>
+        /// prompts the user with atm usage options, takes users input and
+        /// passes it the the HandleOption method 
+        /// </summary>
+        /// <param name="balance">decimal type of the users current account balance</param>
         public static void UserInterface(decimal balance)
         {
             Console.WriteLine("1. View Balance");
@@ -30,6 +35,12 @@ namespace BankATM
             
         }
 
+        /// <summary>
+        /// calls on the proper method for viewing balance, making a withdrawl, making a 
+        /// deposit or exiting the ATM
+        /// </summary>
+        /// <param name="val">a byte that is either 1, 2, 3 or 0</param>
+        /// <param name="balance">decimal type of the users current account balance</param>
         public static void HandleOption(byte val, decimal balance)
         {
 
@@ -46,7 +57,12 @@ namespace BankATM
                 {
                     Console.WriteLine("How much would you like to withdrawl?");
                     decimal withdrawl = Convert.ToDecimal(Console.ReadLine());
-                    decimal currentBalance = MakeWithdrawl(withdrawl, balance);
+                    if (withdrawl < 1)
+                    {
+                        Console.WriteLine("Please enter a valid amount");
+                        HandleOption(val, balance);
+                    }
+                    decimal currentBalance = MakeWithdrawl(withdrawl, balance);     //updates the balance
                     UserInterface(currentBalance);
                 }
                 catch (Exception)
@@ -61,7 +77,12 @@ namespace BankATM
                 {
                     Console.WriteLine("How much would you like to deposit?");
                     decimal deposit = Convert.ToDecimal(Console.ReadLine());
-                    decimal currentBalance = MakeDeposit(deposit, balance);
+                    if (deposit < 1)
+                    {
+                        Console.WriteLine("Please enter a valid amount");
+                        HandleOption(val, balance);
+                    }
+                    decimal currentBalance = MakeDeposit(deposit, balance);     //updates the balance
                     UserInterface(currentBalance);
                 }
                 catch (Exception)
@@ -76,11 +97,27 @@ namespace BankATM
             }
         }
 
+        /// <summary>
+        /// takes in the balance and returns the balance
+        /// </summary>
+        /// <param name="balance">decimal type of the users account balance</param>
+        /// <returns>returns the decimal type of the users account balance</returns>
         public static decimal ViewBalance(decimal balance)
         {
             return balance;
         }
 
+        /// <summary>
+        /// takes in a requested withdrawl amount and the current balance, subtracts
+        /// withdrawl amount from balance and checks to see if the result is greater than
+        /// zero. If not greater than zero, it alerts the user to their balance and terminates
+        /// the withdrawl. If it is greater than zero, it shows the user the updated balance,
+        /// then returns the new balance.
+        /// </summary>
+        /// <param name="amount">decimal type of how much the user wants to withdraw</param>
+        /// <param name="balance">decimal type of the users current account balance</param>
+        /// <returns>if balance would be less than zero, returns decimal type of the current 
+        /// balance. if balance will be zero or greater, decimal type of the new balance is returned</returns>
         public static decimal MakeWithdrawl(decimal amount, decimal balance)
         {
             decimal newBalance = balance - amount;
@@ -100,7 +137,15 @@ namespace BankATM
             return newBalance;
 
         }
-            
+
+        /// <summary>
+        /// takes in a request deposit amout and the current balance, the
+        /// two are then added together, the user is prompted with their
+        /// updated balance, and the new balance is returned.
+        /// </summary>
+        /// <param name="deposit">decimal type of the amount the user wants to deposit</param>
+        /// <param name="balance">decimal type of the users current account balance</param>
+        /// <returns>decimal type of the new balance after the deposited amount was added</returns>
         public static decimal MakeDeposit(decimal deposit, decimal balance)
         {
             decimal newBalance = deposit + balance;
--- Readme.md
+++ Readme.md
@@ -1,70 +1,44 @@
-![cf](http://i.imgur.com/7v5ASc8.png) Lab 2: Unit Testing
-=====================================
+# Lab02 Phil Werner
+## ATM Machine
+A very simple ATM that allows a user to view their balance, make
+a withdrawl, deposit money into their account and exit the program.
+You can view, deposit, withdraw as many times as you want.
+## Tools Used
+Microsoft Visual Studio Community Version 15.5.7
 
-## To Submit this Assignment
+C#
 
-- fork this repository
-- create a new branch named `lab02-`; + `<your name>` **e.g.** `lab02-amanda`
-- write your code
-- push to your repository
-- submit a pull request to this repository
-- submit a link to your PR in canvas
-- Include a Readme.md (contents described below)
+ASP.Net
 
-## Directions
+XUnit
 
-### The Problem Domain
+## Getting Started
 
-- Create a bank "ATM" machine within a console application with VS2017
-- Your solution should include the following:
-    - Main method
-	- Testable external methods
-	- User interface that prompts the user for standard ATM operations
-		- View Balance
-		- Withdraw Money
-		- Add Money (not standard, but for this exercise it will be....)
-	- Exception Handling (try/catch/finally)
-	- Keep asking the user to choose a transaction until they choose to 'exit' the application
-- A few things to keep in mind:
-	- Make sure the user can't go below a zero balance
-	- You cannot unit test methods that ask for user input
-	- Be sure your program class is `public`
-	- Make sure your methods are just returning values and not reading input from the console. 
+Clone this repository to your local machine.
+```
+$ git clone 
+```
+Once downloaded, cd into the ```Lab02-Unit-Testing``` directory.
+```
+$ cd Lab02-Unit-Testing
+```
+The cd into ```BankATM``` directory.
+```
+$ cd BankATM
+```
+The cd into the second ```BankATM``` directory.
+```
+$ cd BankATM
+```
+Then run .NET build.
+```
+$ dotnet build
+```
+Once that is complete, run the program.
+```
+$ dotnet run
+```
 
-- **Stay within scope of the what we have learned so far in the class.**
-	- if we have not officially learned the concept you are trying to implement, then don't use it. 
-	- Ask the instructor or TA if you have any questions about the scope. 
+## Demo
 
-### Unit Tests
-1. Provide unit tests for your ATM.
-2. For every method/action (that does not require user input), there should be a passing valid input.
-1. Although I am not requiring a specific number of tests, i expect you to have no less than 10. 
-3. **A failing test, is not a valid test** - even if the failure is expected. 
-
-
-### Stretch
-
-- Record each transaction the user records and when they exit, provide a receipt for every transaction they conducted. 
-
-## ReadMe
-A README is a module consumer's first -- and maybe only -- look into your creation. The consumer wants a module to fulfill their need, so you must explain exactly what need your module fills, and how effectively it does so.
-<br />
-Your job is to
-
-1. tell them what it is (with context)
-2. show them what it looks like in action
-3. show them how they use it
-4. tell them any other relevant details
-<br />
-
-This is ***your*** job. It's up to the module creator to prove that their work is a shining gem in the sea of slipshod modules. Since so many developers' eyes will find their way to your README before anything else, quality here is your public-facing measure of your work.
-
-<br /> Refer to the sample-README in the class repo `Resources` folder for an example. 
-- [Reference](https://github.com/noffle/art-of-readme)
-
-
-## Rubric
-
-- 7pts: Program meets all requirements described in Lab directions
-- 3pts: Code meets industry standards
-- **Readme.md AND tests required for submission. Missing readme document and tests will result in a best score of 2/10**
+Here are some sample images of it running:
